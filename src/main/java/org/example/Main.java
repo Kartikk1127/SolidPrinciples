0b1d0a827fbc019842f0f80ecd61e0684bc2f88e
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2025-01-25 11:02:01 +0530

    removed unnecessary cod3e

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,7 @@
 package org.example;
 
-import org.example.srp.BankService;
-
 public class Main {
     public static void main(String[] args) {
-        //if you want to deposit money
-        BankService bankService = new BankService(0,"");
-        bankService.deposit(10000,"1234567890");
-        bankService.withdraw(1000,"1234567890");
-
-        System.out.println(bankService.getAmount("1234567890"));
 
     }
 }
--- src/main/java/org/example/srp/PrinterService.java
+++
@@ -1,7 +0,0 @@
-package org.example.srp;
-
-public class PrinterService {
-    public void printPassbook(){
-        //update the transaction info in the passbook
-    }
-}
