8b16b8c5b66d991a00b0efa9c0d23e9b2e1eba02
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2025-01-25 10:48:29 +0530

    single responsibility principle applied for separation of concerns

--- src/main/java/org/example/srp/BankService.java
+++ src/main/java/org/example/srp/BankService.java
@@ -3,40 +3,29 @@ package org.example.srp;
 //Single Responsibility Principle: Every java class must perform a single functionality.
 //Implementation of multiple functionalities in a single class mess the code up
 
-//example of a bad code
 public class BankService {
+
+    long amount;
+    String accountNo;
+    long currentAmount;
+    public BankService(long amount, String accountNo){
+        this.amount=amount;
+        this.accountNo=accountNo;
+    }
     public long deposit(long amount, String accountNo){
         //deposit amount
-        return 0;
+        currentAmount = getAmount(accountNo);
+        currentAmount+=amount;
+        return currentAmount;
+    }
+
+    public long getAmount(String accountNo) {
+        return currentAmount;
     }
 
     public long withdraw(long amount, String accountNo){
-        //withdraw amount
-        return 0;
-    }
-
-    public void printPassbook(){
-        //update the transaction info in the passbook
-    }
-
-    //in future, for loans like education loan or let's say business loan this class has to be modified to be able to support
-    //all the loans
-    public void getLoanInterestInfo(String loanType){
-        if (loanType.equals("homeLoan")){
-            //do some job
-        }
-        if (loanType.equals("personalLoan")){
-            //do some job
-        }
-        if (loanType.equals("car")){
-            //do some job
-        }
-    }
-
-    //in future, if otp sending is done through sms, this class has to be modified in order to be able to support it.
-    public void sendOtp(String medium){
-        if (medium.equals("email")){
-            //java smtp to send emails
-        }
+        if (amount>currentAmount) return -1;
+        currentAmount-=amount;
+        return currentAmount;
     }
 }
