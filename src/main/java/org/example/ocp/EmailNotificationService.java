924d643776efae1ea148d26758cfab6e76445692
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2025-01-25 11:29:21 +0530

    pushed untracked files

---
+++ src/main/java/org/example/ocp/EmailNotificationService.java
@@ -0,0 +1,13 @@
+package org.example.ocp;
+
+public class EmailNotificationService implements NotificationService{
+    @Override
+    public void sendOtp(String medium) {
+        //write logic to integrate with the email api
+    }
+
+    @Override
+    public void sendTransactionReport(String medium) {
+        //write logic to integrate with the email api
+    }
+}
