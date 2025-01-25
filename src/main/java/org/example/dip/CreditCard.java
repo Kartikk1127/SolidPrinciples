9d4e19e3455061f69ce6731448a067b8cbbd5475
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2025-01-25 13:34:32 +0530

    implementation of dependency inversion principle

---
+++ src/main/java/org/example/dip/CreditCard.java
@@ -0,0 +1,8 @@
+package org.example.dip;
+
+public class CreditCard implements BankCard{
+
+    public void doTransaction(long amount){
+        System.out.println("Payment using credit card");
+    }
+}
