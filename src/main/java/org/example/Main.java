ec30c01daf9b25c90c4eae3c1ace98ff21698381
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2025-01-25 11:05:20 +0530

    added definition of open closed principle

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        //open closed principle states  that according to new requirements the module should be open for extension but closed for modification
+
     }
 }
